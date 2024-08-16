3034fa97629227326a16d9864e503592f4b4137f
IvElGrimm <126293620+Ivaname99@users.noreply.github.com>
2024-08-16 16:09:05 -0600

    Gitaso

---
+++ EjercicioRetroalimentacion/Data.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioRetroalimentacion
+{
+    public class Data
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return "Data Source=CC1PC16-17;Initial Catalog=AdventureWorks2016;Integrated Security=True";
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            var conexion = new SqlConnection(ConnectionString);
+                conexion.Open();
+                return conexion;
+            
+        }
+
+        public Customers obtenertodos()
+        {
+            using (var conexion = getConnection() )
+            {
+                String Otodos = "";
+                Otodos = Otodos + "SELECT [CategoryID] " + "\n";
+                Otodos = Otodos + "      ,[CategoryName] " + "\n";
+                Otodos = Otodos + "      ,[Description] " + "\n";
+                Otodos = Otodos + "      ,[Picture] " + "\n";
+                Otodos = Otodos + "  FROM [dbo].[Categories]";
+
+
+
+
+
+                using (var command = SqlCommand(Otodos, conexion))
+                {
+
+                }
+            }
+        }
+    }
+}
